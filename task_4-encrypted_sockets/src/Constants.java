52031a2f26f705432bba9ddaacbe3d2c32c78b98
Fredrik Öberg <fredrik.oberg@gmx.com>
2020-11-30 12:44:34 +0100

    test

--- task_4-encrypted_sockets/src/Constants.java
+++ task_4-encrypted_sockets/src/Constants.java
@@ -1,7 +1,7 @@
 public class Constants {
 
-    public static final String USERNAME = "";
-    public static final String PASSWORD = "";
+    public static final String USERNAME = "test";
+    public static final String PASSWORD = "test";
     public static final String HOST = "webmail.kth.se";
     public static final String[] CIPHER = {"TLS_RSA_WITH_AES_256_CBC_SHA"};
     public static final int PORT = 993;
